b73f746bb54b333619e2f82a4360b6456f1009e1
hborcher <hborcher@illinois.edu>
2017-08-21 16:49:55 -0500

    Added sample fixture for the Package class

--- tests/test_packages_model.py
+++ tests/test_packages_model.py
@@ -7,6 +7,13 @@ def packages():
     return pkgs.Packages()
     pass
 
+@pytest.fixture()
+def sample_package():
+    my_packages = pkgs.Packages()
+    my_packages.add_package(path="c://temp/foo")
+    return my_packages["c://temp/foo"]
+
+
 
 def test_package_empty_len(packages):
     assert len(packages) == 0
@@ -29,3 +36,7 @@ def test_alert_dup_added(packages):
     packages.add_package(path="c://temp/foo")
     with pytest.raises(FileExistsError):
         packages.add_package(path="c://temp/foo")
+
+
+def test_package_size(sample_package):
+    assert len(sample_package) == 0
