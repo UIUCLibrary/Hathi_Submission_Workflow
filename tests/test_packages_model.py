1427d4fc7f428972bfe9a24edb5f0ba143c9d34f
hborcher <hborcher@illinois.edu>
2017-08-22 13:22:33 -0500

    updated tests testing the Package class since it's now a Collection type

--- tests/test_packages_model.py
+++ tests/test_packages_model.py
@@ -7,29 +7,38 @@ def packages():
     return pkgs.Packages()
     pass
 
+
 @pytest.fixture()
 def sample_package():
     my_packages = pkgs.Packages()
-    my_packages.add_package(path="c://temp/foo")
-    return my_packages["c://temp/foo"]
+    my_packages.add_package(path="c:/temp/foo")
+    return my_packages["c:/temp/foo"]
 
+
+@pytest.fixture()
+def sample_package_with_items():
+    my_packages = pkgs.Packages()
+    my_packages.add_package(path="c:/temp/foo")
+    my_package = my_packages["c:/temp/foo"]
+    my_package._items.append("c:/temp/foo/spam0001.tif")
+    return my_package
 
 
 def test_package_empty_len(packages):
     assert len(packages) == 0
 
 
-def test_add_file_entry(packages):
-    packages.add_package(path="c://temp/foo")
+def test_add_package_entry(packages):
+    packages.add_package(path="c:/temp/foo")
     assert len(packages) == 1
 
 
 def test_set_and_retrieve_entry(packages):
-    packages.add_package(path="c://temp/foo")
+    packages.add_package(path="c:/temp/foo")
     assert len(packages) == 1
-    spam = packages["c://temp/foo"]
-    assert spam.path == "c://temp/foo"
-    assert spam["pages"] == []
+    spam = packages["c:/temp/foo"]
+    assert spam.path == "c:/temp/foo"
+    assert len(spam) == 0
 
 
 def test_alert_dup_added(packages):
