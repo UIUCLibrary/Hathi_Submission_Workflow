d03b39320e9028a5b5584df427814c1b3b7148f5
Henry Borchers <henryborchers@yahoo.com>
2017-08-23 16:37:00 -0500

    Load in and use the PackageModel

--- hsw/ui/packageBrowser.py
+++ hsw/ui/packageBrowser.py
@@ -1,9 +1,13 @@
-from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QWidget
 from .ui_packages import Ui_PackagesDialog
-class PackageBroswer(QtWidgets.QWidget, Ui_PackagesDialog):
-    def __init__(self, path) -> None:
+from .packagesModel import PackageModel
+from hsw.packages import Packages
+
+class PackageBrowser(QWidget, Ui_PackagesDialog):
+    def __init__(self, packages: Packages) -> None:
         super().__init__()
         self.setupUi(self)
-        self.root = path
+        self.model = PackageModel(packages)
+        self.packageView.setModel(self.model)
+        self.root = packages.root
         self.show()
-
