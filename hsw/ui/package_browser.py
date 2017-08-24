91e367841d7f2dc9d20bd0bb27217dea027bcd97
Henry Borchers <henryborchers@yahoo.com>
2017-08-24 16:28:49 -0500

    Use the FileDelegate for selecting title page

--- hsw/ui/package_browser.py
+++ hsw/ui/package_browser.py
@@ -1,7 +1,8 @@
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QTreeView
 from .ui_packages import Ui_PackagesDialog
 from .packages_model import PackageModel
 from hsw.package_list import PackagesList
+from .package_files_delegate import FileSelectionDelegate
 
 class PackageBrowser(QWidget, Ui_PackagesDialog):
     def __init__(self, packages: PackagesList) -> None:
@@ -9,5 +10,6 @@ class PackageBrowser(QWidget, Ui_PackagesDialog):
         self.setupUi(self)
         self.model = PackageModel(packages)
         self.packageView.setModel(self.model)
+        self.packageView.setItemDelegateForColumn(1, FileSelectionDelegate(self))
         self.root = packages.root
         self.show()
