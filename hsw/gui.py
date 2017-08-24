d97e802e65aa73a12fe44be391604f78bbf05a23
Henry Borchers <henryborchers@yahoo.com>
2017-08-24 14:56:28 -0500

    Use PackagesList instead of Packages

--- hsw/gui.py
+++ hsw/gui.py
@@ -2,16 +2,17 @@ import sys
 import os
 from PyQt5.QtWidgets import QApplication
 from hsw.ui import PackageBrowser
-from hsw.packages import Packages
+from hsw.package_list import PackagesList
 
 def main():
     app = QApplication(sys.argv)
     # TODO setup args
     try:
-        my_packages = Packages(root=sys.argv[1])
+        my_packages = PackagesList(root=sys.argv[1])
         for path in filter(lambda item: item.is_dir(), os.scandir(sys.argv[1])):
             my_packages.add_package(path.path)
-        print(len(my_packages))
+
+        # noinspection PyUnusedLocal
         window = PackageBrowser(packages=my_packages)
     except IndexError:
         print("Missing path")
