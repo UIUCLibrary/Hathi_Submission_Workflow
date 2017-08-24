fd9a55dc79876394b2360cdd2b24212ec1ac43a7
Henry Borchers <henryborchers@yahoo.com>
2017-08-24 14:59:14 -0500

    Use PackageList instead of Packages

--- hsw/ui/packageBrowser.py
+++ hsw/ui/packageBrowser.py
@@ -1,10 +1,10 @@
 from PyQt5.QtWidgets import QWidget
 from .ui_packages import Ui_PackagesDialog
 from .packagesModel import PackageModel
-from hsw.packages import Packages
+from hsw.package_list import PackagesList
 
 class PackageBrowser(QWidget, Ui_PackagesDialog):
-    def __init__(self, packages: Packages) -> None:
+    def __init__(self, packages: PackagesList) -> None:
         super().__init__()
         self.setupUi(self)
         self.model = PackageModel(packages)
