bf936e2448c35f6543c1b4d08c138355f1b155c3
hborcher <hborcher@illinois.edu>
2017-09-07 15:37:38 -0500

    added clean

--- make.bat
+++ make.bat
@@ -6,6 +6,7 @@ if "%1" == "install-dev"    goto install-dev
 if "%1" == "gui"            goto gui
 if "%1" == "test"           goto test
 if "%1" == "release"        goto release
+if "%1" == "clean"        goto clean
 EXIT /B 0
 
 :main
@@ -38,10 +39,19 @@ goto :eof
     echo Creating standalone release
     setlocal
     set "VSCMD_START_DIR=%CD%"
-    call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-    REM call "%vs140comntools%..\..\VC\vcvarsall.bat" x86_amd64
-    REM nuget install packages.config
-    MSBuild make.proj
+    REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+    call "%vs140comntools%..\..\VC\vcvarsall.bat" x86_amd64
+    nuget install packages.config -OutputDirectory build\nugetpackages
+    MSBuild make.proj /t:msi
     endlocal
 
+goto :eof
+
+:clean
+    python setup.py clean
+    setlocal
+    set "VSCMD_START_DIR=%CD%"
+    call "%vs140comntools%..\..\VC\vcvarsall.bat" x86_amd64
+    MSBuild make.proj /t:clean
+    endlocal
 goto :eof
