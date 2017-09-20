8cfac63ace148b2cc92f48ae2972092af259eea3
hborcher <hborcher@illinois.edu>
2017-09-20 09:18:14 -0500

    added docs and help targets

--- make.bat
+++ make.bat
@@ -4,16 +4,31 @@
 if [%1] == []               goto main
 if "%1" == "install-dev"    goto install-dev
 if "%1" == "gui"            goto gui
+if "%1" == "docs"           goto docs
 if "%1" == "venv"           goto venv
 if "%1" == "venvclean"      goto venvclean
 if "%1" == "test"           goto test
 if "%1" == "release"        goto release
 if "%1" == "clean"          goto clean
+if "%1" == "help"           goto help
+
 EXIT /B 0
 
 :main
     call :install-dev
     call :gui
+goto :eof
+
+:help
+    echo Available options:
+    echo     make install-dev       Installs the development requirements into active python environment
+    echo     make venv              Creates a virtualenv with development requirements
+    echo     make venvclean         Removes the generated virtualenv
+    echo     make gui               Generates GUI source for all .ui files in the ./ui directory
+    echo     make docs              Generates html documentation into the docs/build/html directory
+    echo     make test              Runs tests
+    echo     make release           Creates a Windows Release build
+    echo     make clean             Removes generated files
 goto :eof
 
 :gui
@@ -42,7 +57,7 @@ goto :eof
 goto :eof
 
 :venv
-    if exist ".env" echo "%CD%\.env" folder already exists. & goto :eof
+    if exist ".env" echo "%CD%\.env" folder already exists. To activate virtualenv, use .env\Scripts\activate.bat & goto :eof
     echo Creating a local virtualenv in "%CD%\.env"
     setlocal
     py -m venv .env
@@ -55,6 +70,14 @@ goto :eof
     if exist ".env" echo removing .env & RD /S /Q .env
 goto :eof
 
+:docs
+    echo Creating docs
+    setlocal
+    if exist ".env" call .env\Scripts\activate.bat
+    python setup.py build_sphinx
+    endlocal
+goto :eof
+
 :release
     echo Creating standalone release
     setlocal
@@ -64,7 +87,7 @@ goto :eof
     if exist ".env" call .env\Scripts\activate.bat
     call "%vs140comntools%..\..\VC\vcvarsall.bat" x86_amd64
     nuget install windows_build\packages.config -OutputDirectory build\nugetpackages
-    MSBuild make.pyproj /t:msi /p:ProjectRoot="%CD%
+    MSBuild release.pyproj /t:msi /p:ProjectRoot="%CD%
     endlocal
 
 goto :eof
@@ -73,10 +96,10 @@ goto :eof
     echo Calling clean for Python
     setlocal
 	if exist ".env" call .env\Scripts\activate.bat
-    python setup.py clean
+    python setup.py clean --all
     
     set "VSCMD_START_DIR=%CD%"
     call "%vs140comntools%..\..\VC\vcvarsall.bat" x86_amd64
-    MSBuild windows_build\make.proj /t:Clean /p:ProjectRoot=%CD%
+    MSBuild release.pyproj /t:Clean /p:ProjectRoot=%CD%
     endlocal
 goto :eof
