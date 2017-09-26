28e031cc5806d1027399d30f875d9b1e2ac70870
hborcher <hborcher@illinois.edu>
2017-09-26 16:49:17 -0500

    added a __str__ method for better understanding

--- hsw/collection.py
+++ hsw/collection.py
@@ -1,9 +1,6 @@
 import abc
 import collections
-import logging
 import typing
-
-import os
 
 
 class AbsPackageComponent(metaclass=abc.ABCMeta):
@@ -43,6 +40,9 @@ class AbsPackageComponent(metaclass=abc.ABCMeta):
     @staticmethod
     def init_local_metadata() -> dict:
         return dict()
+
+    def __str__(self) -> str:
+        return "{} {}".format(super().__str__(), dict(self.metadata))
 
 
 class Collection(AbsPackageComponent):
@@ -92,30 +92,3 @@ class Instantiation(AbsPackageComponent):
         self.parent.instantiations[self.category] = child
 
 
-def build_bb_instance(new_item, path, name):
-    new_instantiation = Instantiation(category="access",parent=new_item)
-    for file in filter(lambda i: i.is_file(), os.scandir(path)):
-        if os.path.splitext(os.path.basename(file))[0] == name:
-            new_instantiation.files.append(file.path)
-
-
-def build_bb_package(new_package, path):
-    logger = logging.getLogger(__name__)
-    files = set(map(lambda item: os.path.splitext(item)[0], os.listdir(path)))
-    for unique_item in sorted(files):
-        logger.debug(unique_item)
-        new_item = Item(parent=new_package)
-        new_item.component_metadata["item_name"] = unique_item
-        build_bb_instance(new_item, name=unique_item, path=path)
-
-
-def build_bb_collection(root) -> Collection:
-    logger = logging.getLogger(__name__)
-    new_collection = Collection(root)
-    for directory in filter(lambda i: i.is_dir(), os.scandir(root)):
-        logger.debug("scanning {}".format(directory.path))
-        new_package = Package(parent=new_collection)
-        new_package.component_metadata['path'] = directory.path
-        new_package.component_metadata["package_type"] = "Brittle Books HathiTrust Submission Package"
-        build_bb_package(new_package, path=directory.path)
-    return new_collection
