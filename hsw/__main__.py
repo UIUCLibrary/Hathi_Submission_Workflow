faea940f5f6bd516404a3ea26ec4903528f4a49d
hborcher <hborcher@illinois.edu>
2017-09-26 15:04:04 -0500

    turned on main logger for debug info for now

--- hsw/__main__.py
+++ hsw/__main__.py
@@ -1,13 +1,18 @@
 import sys
 
 from hsw import gui
-
+import logging
 
 def main():
     if len(sys.argv) > 1 and sys.argv[1] == "--pytest":
         import pytest  # type: ignore
         sys.exit(pytest.main(sys.argv[2:]))
     else:
+        logging.root.setLevel(logging.DEBUG)
+        logger = logging.getLogger(__name__)
+        std_handler = logging.StreamHandler(sys.stdout)
+        logging.root.addHandler(std_handler)
+        logger.debug("Debug on")
         gui.main()
 
 
