46b3b2a584b4a0dd97b99c8a6597f64d7fc78a03
hborcher <hborcher@illinois.edu>
2017-09-27 18:04:46 -0500

    added prep, validate, and zip steps

--- hsw/workflow.py
+++ hsw/workflow.py
@@ -1,7 +1,9 @@
 import abc
 from . import collection
 from . import collection_builder
-
+from pyhathiprep import package_creater
+from hathi_validate import process as validate_process
+from hathizip import process as zip_process
 
 class AbsStrategy(metaclass=abc.ABCMeta):
     @abc.abstractmethod
@@ -18,6 +20,7 @@ class DSStrategy(AbsStrategy):
     def build_collection(self, root) -> collection.Package:
         return collection_builder.build_ds_collection(root)
 
+
 class Workflow:
     def __init__(self, strategy: AbsStrategy) -> None:
         self._strategy = strategy
@@ -26,4 +29,20 @@ class Workflow:
         return self._strategy.build_collection(root)
 
     def prep(self, package):
-        print("prepping")
+        for package_object in package:
+            package_builder = package_creater.InplacePackage(package_object.metadata['path'])
+            package_builder.generate_package()
+            # package_creater.create_package(source=package_object.metadata['path'])
+
+    def validate(self, package):
+        print("Validating")
+        errors = []
+        for package_object in package:
+            errors += validate_process.process_directory(package_object.metadata['path'])
+        return errors
+
+    def zip(self, package, destination):
+        print("zipping")
+        for package_object in package:
+            print(package_object)
+            zip_process.compress_folder(package_object.metadata['path'], destination)
