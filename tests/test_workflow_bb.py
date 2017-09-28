870821ccdc91a8fd003950ddd8f2d019e7d7b062
hborcher <hborcher@illinois.edu>
2017-09-28 17:26:36 -0500

    Generate the basic meta.yml file

--- tests/test_workflow_bb.py
+++ tests/test_workflow_bb.py
@@ -1,6 +1,6 @@
 import pytest
 import os
-from hsw import collection_builder
+from hsw import collection_builder,workflow
 
 
 @pytest.fixture(scope="session")
@@ -40,6 +40,7 @@ def bb_collection(tmpdir_factory):
         os.path.join("1251150", "00000010.jp2"),
         os.path.join("1251150", "00000010.txt"),
         os.path.join("1251150", "00000010.xml"),
+
         os.path.join("1790923", "00000001.jp2"),
         os.path.join("1790923", "00000001.txt"),
         os.path.join("1790923", "00000001.xml"),
@@ -55,13 +56,32 @@ def bb_collection(tmpdir_factory):
     ]
     tests_files_dir = tmpdir_factory.mktemp("BB_Test")
     for package_name in brittle_books_packages:
-        os.mkdir(os.path.join(tests_files_dir, package_name))
+
+        new_package_folder = os.path.join(tests_files_dir, package_name)
+        os.mkdir(new_package_folder)
+
+        with open(os.path.join(new_package_folder, "checksum.md5"), "w") as f:
+            pass
+        with open(os.path.join(new_package_folder, "marc.xml"), "w") as f:
+            pass
+        with open(os.path.join(new_package_folder, "meta.yml"), "w") as f:
+            f.write("capture_date: 2016-06-21T08:00:00Z\n")
+            f.write("capture_agent: TRIGONIX\n")
+            f.write("scanner_user: TRIGONIX\n")
+            f.write("scanner_make: TRIGONIX\n")
+            f.write("scanner_model: TRIGO-C1\n")
+
+
     for test_file in brittle_books_package_files:
         new_test_item = tests_files_dir.join(test_file)
         with open(new_test_item, "w") as f:
             pass
-    return collection_builder.build_bb_collection(tests_files_dir)
+    return str(tests_files_dir)
 
 
 def test_prep(bb_collection):
     print(bb_collection)
+    my_workflow = workflow.Workflow(workflow.BrittleBooksWorkflow())
+    new_package = my_workflow.build_package(bb_collection)
+    errors = my_workflow.validate(new_package)
+    assert len(errors) == 0
