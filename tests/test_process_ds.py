eb18337bc56cb2fe3529a6de0aaa82010ee806c6
hborcher <hborcher@illinois.edu>
2017-09-28 13:37:18 -0500

    test that item validates

--- tests/test_process_ds.py
+++ tests/test_process_ds.py
@@ -84,7 +84,99 @@ def ds_collection(tmpdir_factory):
     ]
     tests_files_dir = tmpdir_factory.mktemp("DS_Test")
     for package_name in ds_collection_packages:
-        os.mkdir(os.path.join(tests_files_dir, package_name))
+        package_path = os.path.join(tests_files_dir, package_name)
+        os.mkdir(package_path)
+        with open(os.path.join(package_path, "marc.xml"), "w") as f:
+            f.write("""<record xmlns="http://www.loc.gov/MARC21/slim" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd">
+  <leader>01342cam a2200277Ka 4500</leader>
+  <controlfield tag="001">2693684</controlfield>
+  <controlfield tag="005">20170511212234.0</controlfield>
+  <controlfield tag="008">900530s1845    it bf         000 0 ita d</controlfield>
+  <datafield ind1=" " ind2=" " tag="035">
+    <subfield code="a">(OCoLC)ocm77313810</subfield>
+  </datafield>
+  <datafield ind1=" " ind2=" " tag="040">
+    <subfield code="a">UIU</subfield>
+    <subfield code="e">dcrmb</subfield>
+    <subfield code="c">UIU</subfield>
+    <subfield code="d">UKMGB</subfield>
+    <subfield code="d">UIU</subfield>
+  </datafield>
+  <datafield ind1=" " ind2=" " tag="019">
+    <subfield code="a">561873437</subfield>
+  </datafield>
+  <datafield ind1=" " ind2=" " tag="035">
+    <subfield code="a">(OCoLC)77313810</subfield>
+    <subfield code="z">(OCoLC)561873437</subfield>
+  </datafield>
+  <datafield ind1=" " ind2=" " tag="043">
+    <subfield code="a">e-it---</subfield>
+  </datafield>
+  <datafield ind1=" " ind2=" " tag="049">
+    <subfield code="a">UIUU</subfield>
+    <subfield code="o">skip</subfield>
+  </datafield>
+  <datafield ind1="1" ind2=" " tag="100">
+    <subfield code="a">Monti, Carlo,</subfield>
+    <subfield code="d">19th cent.</subfield>
+  </datafield>
+  <datafield ind1="1" ind2="0" tag="245">
+    <subfield code="a">Studio topografico intorno alla piu&#768; breve congiunzione stradale fra i due mari nell'alta Italia merce&#768; un varco esistente nel tronco settentrionale dell'Apennino :</subfield>
+    <subfield code="b">memoria /</subfield>
+    <subfield code="c">dell'avvocato Carlo Monti.</subfield>
+  </datafield>
+  <datafield ind1=" " ind2=" " tag="260">
+    <subfield code="a">Bologna :</subfield>
+    <subfield code="b">Tipi governativi alla Volpe,</subfield>
+    <subfield code="c">1845.</subfield>
+  </datafield>
+  <datafield ind1=" " ind2=" " tag="300">
+    <subfield code="a">48 p., [1] folded leaf of plates :</subfield>
+    <subfield code="b">map ;</subfield>
+    <subfield code="c">27 cm.</subfield>
+  </datafield>
+  <datafield ind1=" " ind2=" " tag="500">
+    <subfield code="a">Cavagna 1693: University of Illinois bookplate: "From the library of Conte Antonio Cavagna Sangiuliani di Gualdana Lazelada di Bereguardo, purchased 1921".</subfield>
+    <subfield code="5">IU-R</subfield>
+  </datafield>
+  <datafield ind1=" " ind2=" " tag="500">
+    <subfield code="a">625.7 M767s: Cavagna Library stamp on p. [3].</subfield>
+    <subfield code="5">IU-R</subfield>
+  </datafield>
+  <datafield ind1=" " ind2="0" tag="650">
+    <subfield code="a">Roads</subfield>
+    <subfield code="z">Italy.</subfield>
+  </datafield>
+  <datafield ind1="1" ind2=" " tag="700">
+    <subfield code="a">Cavagna Sangiuliani di Gualdana, Antonio,</subfield>
+    <subfield code="c">conte,</subfield>
+    <subfield code="d">1843-1913,</subfield>
+    <subfield code="e">former owner.</subfield>
+    <subfield code="5">IU-R</subfield>
+  </datafield>
+  <datafield ind1="2" ind2=" " tag="710">
+    <subfield code="a">Cavagna Collection (University of Illinois at Urbana-Champaign Library)</subfield>
+    <subfield code="5">IU-R</subfield>
+  </datafield>
+  <datafield ind1=" " ind2=" " tag="752">
+    <subfield code="a">Italy</subfield>
+    <subfield code="d">Bologna.</subfield>
+  </datafield>
+  <datafield ind1="4" ind2="1" tag="856">
+    <subfield code="u">https://hdl.handle.net/2027/uiug.30112088620619</subfield>
+    <subfield code="3">HathiTrust Digital Library</subfield>
+  </datafield>
+  <datafield ind1=" " ind2=" " tag="994">
+    <subfield code="a">C0</subfield>
+    <subfield code="b">UIU</subfield>
+  </datafield>
+  <datafield ind1=" " ind2=" " tag="955">
+    <subfield code="b">2693684</subfield>
+  </datafield>
+</record>""")
+
+
+
     for test_file in ds_collection_package_files:
         new_test_item = tests_files_dir.join(test_file)
         with open(new_test_item, "w") as f:
@@ -103,7 +195,9 @@ def test_prep(ds_collection):
 
     prepped_package = processor.build_package(new_package.path)
     errors = processor.validate(prepped_package)
-    print(errors)
+    for error in errors:
+        print(error)
+    assert len(errors) == 0
     with tempfile.TemporaryDirectory() as temp_destination:
         processor.zip(prepped_package, temp_destination)
         assert os.path.exists(os.path.join(temp_destination, "1564651.zip"))
