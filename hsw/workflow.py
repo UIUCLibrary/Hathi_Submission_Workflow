12d4ce12d93dd0e3c48f4ae18d4cb589def68872
hborcher <hborcher@illinois.edu>
2017-09-28 13:49:47 -0500

    Workflow class is more of a template class instead of a strategy

--- hsw/workflow.py
+++ hsw/workflow.py
@@ -5,44 +5,53 @@ from pyhathiprep import package_creater
 from hathi_validate import process as validate_process
 from hathizip import process as zip_process
 
-class AbsStrategy(metaclass=abc.ABCMeta):
+
+class AbsWorkflow(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def build_collection(self, root) -> collection.Package:
         pass
 
-
-class BrittleBooksStrategy(AbsStrategy):
-    def build_collection(self, root) -> collection.Package:
-        return collection_builder.build_bb_collection(root)
-
-
-class DSStrategy(AbsStrategy):
-    def build_collection(self, root) -> collection.Package:
-        return collection_builder.build_ds_collection(root)
-
-
-class Workflow:
-    def __init__(self, strategy: AbsStrategy) -> None:
-        self._strategy = strategy
-
-    def build_package(self, root):
-        return self._strategy.build_collection(root)
-
-    def prep(self, package):
+    @staticmethod
+    def prep(package):
         for package_object in package:
             package_builder = package_creater.InplacePackage(package_object.metadata['path'])
             package_builder.generate_package()
-            # package_creater.create_package(source=package_object.metadata['path'])
 
-    def validate(self, package):
-        print("Validating")
+    @staticmethod
+    def validate(package):
         errors = []
         for package_object in package:
             errors += validate_process.process_directory(package_object.metadata['path'])
         return errors
 
-    def zip(self, package, destination):
-        print("zipping")
+    @staticmethod
+    def zip(package, destination):
         for package_object in package:
-            print(package_object)
             zip_process.compress_folder(package_object.metadata['path'], destination)
+
+
+class BrittleBooksWorkflow(AbsWorkflow):
+    def build_collection(self, root) -> collection.Package:
+        return collection_builder.build_bb_collection(root)
+
+
+class DSWorkflow(AbsWorkflow):
+    def build_collection(self, root) -> collection.Package:
+        return collection_builder.build_ds_collection(root)
+
+
+class Workflow:
+    def __init__(self, workflow: AbsWorkflow) -> None:
+        self._template = workflow
+
+    def build_package(self, root):
+        return self._template.build_collection(root)
+
+    def prep(self, package):
+        return self._template.prep(package)
+
+    def validate(self, package):
+        return self._template.validate(package)
+
+    def zip(self, package, destination):
+        return self._template.zip(package, destination)
