315b338bc5bcaf733456d10d73fffa834e6c4be0
hborcher <hborcher@illinois.edu>
2017-10-02 15:20:58 -0500

    AbsWorkflow classes returns lists of closures as tasks

--- hsw/workflow.py
+++ hsw/workflow.py
@@ -4,6 +4,7 @@ from . import collection_builder
 from pyhathiprep import package_creater
 from hathi_validate import process as validate_process
 from hathizip import process as zip_process
+import typing
 
 
 class AbsWorkflow(metaclass=abc.ABCMeta):
@@ -12,20 +13,29 @@ class AbsWorkflow(metaclass=abc.ABCMeta):
         pass
 
     @staticmethod
-    def prep(package):
+    def prep(package) -> typing.List[typing.Callable]:
         pass
 
     @staticmethod
-    def validate(package):
-        errors = []
+    def validate(package) -> typing.List[typing.Callable]:
+        # errors = []
+        closures = []
         for package_object in package:
-            errors += validate_process.process_directory(package_object.metadata['path'])
-        return errors
+            def create_task(path):
+                return validate_process.process_directory(path)
+
+            closures.append(lambda p=package_object.metadata['path']: create_task(p))
+        return closures
 
     @staticmethod
     def zip(package, destination):
+        closures = []
         for package_object in package:
-            zip_process.compress_folder(package_object.metadata['path'], destination)
+            def zip_creater(path, output):
+                return zip_process.compress_folder(path, output)
+
+            closures.append(lambda p=package_object.metadata['path'], o=destination: zip_creater(p, o))
+        return closures
 
 
 class BrittleBooksWorkflow(AbsWorkflow):
@@ -33,11 +43,15 @@ class BrittleBooksWorkflow(AbsWorkflow):
         return collection_builder.build_bb_collection(root)
 
     @staticmethod
-    def validate(package):
-        errors = []
+    def validate(package) -> typing.List[typing.Callable]:
+        # errors = []
+        closures = []
         for package_object in package:
-            errors += validate_process.process_directory(package_object.metadata['path'], require_page_data=False)
-        return errors
+            def create_task(path):
+                return validate_process.process_directory(path)
+
+            closures.append(lambda path=package_object.metadata['path']: create_task(path))
+        return closures
 
 
 class DSWorkflow(AbsWorkflow):
@@ -45,12 +59,19 @@ class DSWorkflow(AbsWorkflow):
         return collection_builder.build_ds_collection(root)
 
     @staticmethod
-    def prep(package):
+    def prep(package) -> typing.List[typing.Callable]:
+        closures = []
         for package_object in package:
-            package_builder = package_creater.InplacePackage(package_object.metadata['path'])
-            title_page = package_object.metadata["title_page"]
-            print(title_page)
-            package_builder.generate_package(title_page=title_page)
+            def package_generator(path, title_page):
+                package_builder = package_creater.InplacePackage(path)
+                # title_page = package_object.metadata["title_page"]
+                print(title_page)
+                package_builder.generate_package(destination=path, title_page=title_page)
+
+            closures.append(lambda path=package_object.metadata['path'],
+                                   title_page=package_object.metadata["title_page"]: package_generator(path,
+                                                                                                       title_page))
+        return closures
 
 
 class Workflow:
@@ -60,11 +81,11 @@ class Workflow:
     def build_package(self, root):
         return self._template.build_collection(root)
 
-    def prep(self, package):
+    def prep(self, package) -> typing.List[typing.Callable]:
         return self._template.prep(package)
 
-    def validate(self, package):
+    def validate(self, package) -> typing.List[typing.Callable]:
         return self._template.validate(package)
 
-    def zip(self, package, destination):
+    def zip(self, package, destination) -> typing.List[typing.Callable]:
         return self._template.zip(package, destination)
