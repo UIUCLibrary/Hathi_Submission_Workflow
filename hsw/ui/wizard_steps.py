4fe628b7617f04fc700bc3755f7689a5b7948cdb
hborcher <hborcher@illinois.edu>
2017-10-03 12:16:47 -0500

    Use a signal and slot method for when building a package is done

--- hsw/ui/wizard_steps.py
+++ hsw/ui/wizard_steps.py
@@ -22,18 +22,23 @@ from hathi_validate import report as hathi_validate_report
 
 
 class LocatingPackagesDialog(QtWidgets.QProgressDialog):
-    def worker(self, finished_callback: typing.Callable, reporter_callback: typing.Callable = None):
+    completed_successfully = QtCore.pyqtSignal()
+
+    def worker(self, finished_callback: typing.Callable = None, reporter_callback: typing.Callable = None):
         package = self.package_builder.build_package(self.root)
         with self.lock:
             self.package = package
-        finished_callback()
+        if finished_callback:
+            finished_callback()
+        self.completed_successfully.emit()
 
     def __init__(self, package_builder, root, *__args):
         super().__init__(*__args)
+        self.completed_successfully.connect(self.all_done)
         self.lock = threading.Lock()
         self.package_builder = package_builder
         self.root = root
-        self.q = queue.Queue()
+        # self.q = queue.Queue()
         self.setRange(0, 0)
         self.package_builder = package_builder
         self.package = None
@@ -41,11 +46,16 @@ class LocatingPackagesDialog(QtWidgets.QProgressDialog):
         self.setCancelButton(None)
         self.setLabelText("Locating your files."
                           "\nThis might take some time depending on the size of the collection.")
-        self.thr = threading.Thread(target=self.worker, args=(self.close,))
+        self.thr = threading.Thread(target=self.worker)
+
+    def all_done(self):
+        self.close()
 
     def close(self):
+        self.thr.join()
+        value = super().close()
         print("Closing")
-        return super().close()
+        return value
 
     def exec_(self):
         self.thr.start()
