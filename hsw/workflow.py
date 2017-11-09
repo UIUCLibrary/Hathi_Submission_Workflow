35c9d1e2221cc2ba3efecb136853e091eacb3443
hborcher <hborcher@illinois.edu>
2017-11-09 12:07:13 -0600

    PSR-62 workflow now has a QC step

--- hsw/workflow.py
+++ hsw/workflow.py
@@ -2,11 +2,17 @@ import abc
 
 import os
 
+import sys
+
 from . import collection
 from . import collection_builder
 from pyhathiprep import package_creater, checksum
 from hathi_validate import process as validate_process
 from hathizip import process as zip_process
+import imgvalidator
+import imgvalidator.validate
+import imgvalidator.report
+from imgvalidator import cli
 import typing
 
 
@@ -17,11 +23,11 @@ class AbsWorkflow(metaclass=abc.ABCMeta):
 
     @staticmethod
     def prep(package) -> typing.List[typing.Callable]:
-        pass
+        return [lambda *args: None]
 
     @staticmethod
     def update_checksums(package) -> typing.List[typing.Callable]:
-        return []
+        return [lambda *args: None]
 
     @staticmethod
     def validate(package) -> typing.List[typing.Callable]:
@@ -43,6 +49,10 @@ class AbsWorkflow(metaclass=abc.ABCMeta):
 
             closures.append(lambda p=package_object.metadata['path'], o=destination: zip_creater(p, o))
         return closures
+
+    @staticmethod
+    def qc(package) -> typing.List[typing.Callable]:
+        return [lambda *args: None]
 
 
 class BrittleBooksWorkflow(AbsWorkflow):
@@ -96,6 +106,56 @@ class DSWorkflow(AbsWorkflow):
                                                                                                        title_page))
         return closures
 
+    @staticmethod
+    def qc(package) -> typing.List[typing.Callable]:
+        closures = []
+        profile_builder = imgvalidator.validation_profile.ProfileBuilder(imgvalidator.get_profile_builder("DSPreservationTiff"))
+        profile = profile_builder.build_profile()
+        # fixme: select the correct file dynamically
+        image_format = [".tif"]
+
+        for package_object in package:
+            for item in package_object:
+                for inst in item:
+                    for file_path in inst.files:
+                        if os.path.splitext(file_path)[1].lower() in image_format:
+                            def qc_package(path, validation_profile):
+                                result = imgvalidator.report.ValidationData(path)
+                                try:
+                                    errors = list(imgvalidator.validate.validate_file(path, validation_profile))
+                                    result.errors = errors
+                                    if errors:
+                                        result.valid = False
+                                    else:
+                                        result.valid = True
+                                except AssertionError as e:
+                                    failure_message = "Validation of {} failed. Reason: {}".format(path, e)
+                                    print(failure_message, file=sys.stderr)
+                                except Exception as e:
+                                    print(e)
+                                    raise
+                                return [result]
+                                # return cli.validate_files()
+                                # print("QCing")
+                                # print(path)
+                                # print(validation_profile)
+
+                            closures.append(lambda path=file_path, validation_profile=profile:
+                                            qc_package(path, validation_profile))
+        return closures
+        # print(package_object)
+        # print(type(package_object))
+
+
+        # try:
+        #     return AbsWorkflow.qc(package)
+        # except Exception as e:
+        #     print(e)
+        #     raise
+        # rv = super().qc(package)
+        # print("QC")
+        # return rv
+
 
 class Workflow:
     def __init__(self, workflow: AbsWorkflow) -> None:
@@ -115,3 +175,6 @@ class Workflow:
 
     def update_checksums(self, package) -> typing.List[typing.Callable]:
         return self._template.update_checksums(package)
+
+    def qc(self, package) -> typing.List[typing.Callable]:
+        return self._template.qc(package)
