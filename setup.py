6e5121f5a2aa6c6b92c58eef01d935a52cfe0a9d
hborcher <hborcher@illinois.edu>
2017-11-16 12:48:38 -0600

    use project metatada from setup.cfg

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 from setuptools import setup
-# from dis
 from setuptools.command.build_py import build_py
-# from distutils.command.build import build
 import os
-import hsw
 
 
 class BuildPyCommand(build_py):
@@ -16,23 +13,9 @@ class BuildPyCommand(build_py):
         build_py.run(self)
 
 
-metadata_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'hsw', '__version__.py')
-metadata = dict()
-with open(metadata_file, 'r', encoding='utf-8') as f:
-    exec(f.read(), metadata)
-
-with open('README.rst', 'r', encoding='utf-8') as readme_file:
-    readme = readme_file.read()
-
 setup(
-    name=metadata["__title__"],
-    version=metadata["__version__"],
+
     packages=['hsw', "hsw.ui"],
-    url=metadata["__url__"],
-    license='University of Illinois/NCSA Open Source License',
-    author=metadata["__author__"],
-    author_email=metadata["__author_email__"],
-    description=metadata["__description__"],
     install_requires=[
         "pyqt5",
         "pyhathiprep >= 0.1.3",
@@ -40,7 +23,6 @@ setup(
         "imgvalidator",
         "HathiZip"
     ],
-    long_description=readme,
     test_suite="tests",
     setup_requires=[
         "pytest-runner",
